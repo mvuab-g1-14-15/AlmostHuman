6972ec49582bb79eec2a311e027a1c0481a039e4
Ruly <raulgg1987@gmail.com>
2014-12-09 21:30:50 +0100

    Animated Models and Instances to Animated Models

---
+++ mvuab/Code/Graphics/AnimatedModels/AnimatedModelsManager.cpp
@@ -0,0 +1,26 @@
+#include "AnimatedModelsManager.h"
+
+CAnimatedModelManager::CAnimatedModelManager()
+{
+
+}
+
+CAnimatedModelManager::~CAnimatedModelManager()
+{
+
+}
+
+CAnimatedCoreModel * CAnimatedModelManager::GetCore(const std::string &Name, const std::string &Path)
+{
+    return 0;
+}
+
+CAnimatedInstanceModel * CAnimatedModelManager::GetInstance(const std::string &Name)
+{
+    return 0;
+}
+
+void CAnimatedModelManager::Load(const std::string &Filename)
+{
+
+}
