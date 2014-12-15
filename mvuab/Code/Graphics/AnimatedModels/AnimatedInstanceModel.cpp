c560bcf49d3e300f0b97bea936f21b5a2038c899
Aljenci <alexjenci@gmail.com>
2014-12-15 17:15:08 +0100

    Plantilla AnimatedInstanceModel

--- mvuab/Code/Graphics/AnimatedModels/AnimatedInstanceModel.cpp
+++ mvuab/Code/Graphics/AnimatedModels/AnimatedInstanceModel.cpp
@@ -1,20 +1,83 @@
+#include "AnimatedModels/AnimatedInstanceModel.h"
 
-#include "GraphicsManager.h"
-#include "AnimatedInstanceModel.h"
-#include "Core.h"
-
-
-CAnimatedInstanceModel::CAnimatedInstanceModel(const std::string &Name, const std::string &CoreName)
+CAnimatedInstanceModel::CAnimatedInstanceModel()
 {
-	SetName(Name);
-	//m_StaticMesh = CCore::GetSingletonPtr()->GetStaticMeshManager()->
+
 }
-	
+
 CAnimatedInstanceModel::~CAnimatedInstanceModel()
 {
+
 }
 
-void CAnimatedInstanceModel::Render(CGraphicsManager *GM)
+void
+CAnimatedInstanceModel::Render(CGraphicsManager *RM)
 {
-	//m_StaticMesh->Render(GM);
+
+}
+
+void
+CAnimatedInstanceModel::RenderModelBySoftware(CGraphicsManager *RM)
+{
+
+}
+
+void
+CAnimatedInstanceModel::Update(float ElapsedTime)
+{
+
+}
+
+void
+CAnimatedInstanceModel::Initialize(CAnimatedCoreModel *AnimatedCoreModel, CGraphicsManager *RM)
+{
+
+}
+
+void
+CAnimatedInstanceModel::Destroy()
+{
+
+}
+
+void
+CAnimatedInstanceModel::ExecuteAction(int Id, float DelayIn, float DelayOut, float WeightTarget, bool AutoLock)
+{
+
+}
+
+void
+CAnimatedInstanceModel::BlendCycle(int Id, float Weight, float DelayIn)
+{
+
+}
+
+void
+CAnimatedInstanceModel::ClearCycle(int Id, float DelayOut)
+{
+
+}
+
+bool
+CAnimatedInstanceModel::IsCycleAnimationActive(int Id) const
+{
+
+}
+
+bool
+CAnimatedInstanceModel::IsActionAnimationActive(int Id) const
+{
+
+}
+
+bool
+CAnimatedInstanceModel::LoadVertexBuffer(CGraphicsManager *RM)
+{
+
+}
+
+void
+CAnimatedInstanceModel::LoadTextures()
+{
+
 }
--- mvuab/Code/Graphics/AnimatedModels/AnimatedInstanceModel.h
+++ mvuab/Code/Graphics/AnimatedModels/AnimatedInstanceModel.h
@@ -1,17 +1,33 @@
-#include "GraphicsManager.h"
-#include "RenderableObject\RenderableObject.h"
+#include "RenderableObject/RenderableObject.h"
+#include "cal3d/cal3d.h"
 
-#include <string>
-
-class CStaticMesh;
+class CAnimatedCoreModel;
+class CTexture;
 
 class CAnimatedInstanceModel : public CRenderableObject
 {
 private:
-	CStaticMesh *m_StaticMesh;
+	CalModel *m_CalModel;
+	CAnimatedCoreModel *m_AnimatedCoreModel;
+	std::vector<CTexture *> m_TextureList;
+	LPDIRECT3DVERTEXBUFFER9 m_pVB;
+	LPDIRECT3DINDEXBUFFER9 m_pIB;
+	int m_NumVtxs;
+	int m_NumFaces;
+	bool LoadVertexBuffer(CGraphicsManager *RM);
+	void LoadTextures();
 public:
-	CAnimatedInstanceModel(const std::string &Name, const std::string &CoreName);
+	CAnimatedInstanceModel();
 	~CAnimatedInstanceModel();
-	void Render(CGraphicsManager *GM);
-};
-
+	void Render(CGraphicsManager *RM);
+	void RenderModelBySoftware(CGraphicsManager *RM);
+	void Update(float ElapsedTime);
+	void Initialize(CAnimatedCoreModel *AnimatedCoreModel, CGraphicsManager *RM);
+	void Destroy();
+	void ExecuteAction(int Id, float DelayIn, float DelayOut, float WeightTarget=1.0f, bool
+		AutoLock=true);
+	void BlendCycle(int Id, float Weight, float DelayIn);
+	void ClearCycle(int Id, float DelayOut);
+	bool IsCycleAnimationActive(int Id) const;
+	bool IsActionAnimationActive(int Id) const;
+};
