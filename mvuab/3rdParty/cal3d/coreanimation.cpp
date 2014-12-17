624072e07e9cc813aa851ddea8addf4e8e35ed8a
Andrei Jianu <andreisystem@gmail.com>
2014-12-17 21:35:28 +0100

    Delete memory leaks from cal3d lib

--- mvuab/3rdParty/cal3d/coreanimation.cpp
+++ mvuab/3rdParty/cal3d/coreanimation.cpp
@@ -22,6 +22,12 @@ CalCoreAnimation::CalCoreAnimation()
 
 CalCoreAnimation::~CalCoreAnimation()
 {
+	std::list<CalCoreTrack *>::iterator iteratorCoreTrack;
+	for(iteratorCoreTrack=m_listCoreTrack.begin();iteratorCoreTrack!=m_listCoreTrack.end();++iteratorCoreTrack)
+	{
+		(*iteratorCoreTrack)->destroy();
+		delete *iteratorCoreTrack;
+	}
 }
 
 /*****************************************************************************/
