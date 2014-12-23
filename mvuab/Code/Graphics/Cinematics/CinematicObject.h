6c306c663d0b8147c1dfaa8972d055d67786bc94
Aljenci <alexjenci@gmail.com>
2014-12-23 16:46:48 +0100

    Base of Cinematic created

---
+++ mvuab/Code/Graphics/Cinematics/CinematicObject.h
@@ -0,0 +1,28 @@
+#ifndef CINEMATIC_OBJECT_H
+#define CINEMATIC_OBJECT_H
+
+#include "CinematicPlayer.h"
+
+#include <vector>
+
+class CCinematicObjectKeyFrame;
+class CRenderableObject;
+class CXMLTreeNode;
+
+class CCinematicObject : public CCinematicPlayer
+{
+private:
+	std::vector<CCinematicObjectKeyFrame *> m_CinematicObjectKeyFrames;
+	size_t m_CurrentKeyFrame;
+	CRenderableObject *m_RenderableObject;
+public:
+	CCinematicObject(CXMLTreeNode &atts);
+	bool IsOk();
+	virtual ~CCinematicObject();
+	void AddCinematicObjectKeyFrame(CCinematicObjectKeyFrame *CinematicObjectKeyFrame);
+	void Update(float ElapsedTime);
+	void Stop();
+	void OnRestartCycle();
+};
+
+#endif // CINEMATIC_OBJECT_H
---
+++ mvuab/Code/Graphics/Cinematics/CinematicObjectKeyFrame.cpp
@@ -0,0 +1,1 @@
+#include "CinematicObjectKeyFrame.h"
