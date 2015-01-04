fd5cc9a44b1f4e9484b4d0c4fb8a66ef49132428
Alejandro Jiménez Encinas <alexjenci@gmail.com>
2015-01-04 15:36:07 +0100

    Cinematic with interpolation (Position OK, rotation and scale ERROR)

--- mvuab/Code/Graphics/Cinematics/CinematicObject.cpp
+++ mvuab/Code/Graphics/Cinematics/CinematicObject.cpp
@@ -4,6 +4,10 @@
 #include "Core.h"
 #include "RenderableObject/RenderableObjectsManager.h"
 #include "GraphicsManager.h"
+#include "Math\LerpAnimator3D.h"
+#include "Math\LerpAnimator1D.h"
+#include "Math\MathTypes.h"
+#include "Math\Vector3.h"
 
 CCinematicObject::CCinematicObject(CXMLTreeNode &atts)
 {
@@ -41,7 +45,7 @@ void CCinematicObject::Update(float ElapsedTime)
 	{
 		if(m_CinematicObjectKeyFrames[i]->GetKeyFrameTime() > m_CurrentTime)
 		{
-			m_CurrentKeyFrame = i;
+			m_CurrentKeyFrame = i-1;
 			break;
 		}
 	}
@@ -57,11 +61,36 @@ void CCinematicObject::Update(float ElapsedTime)
 			Pause();
 		}
 	}
-	m_RenderableObject->SetPosition(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetPosition());
-	m_RenderableObject->SetYaw(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetRoll());
-	m_RenderableObject->SetPitch(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetPitch());
-	m_RenderableObject->SetRoll(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetRoll());
-	m_RenderableObject->SetScale(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetScale());
+
+	float l_CurrentP = (m_CurrentTime-m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetKeyFrameTime())/(m_CinematicObjectKeyFrames[m_CurrentKeyFrame+1]->GetKeyFrameTime()-m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetKeyFrameTime());
+
+	Math::CLerpAnimator3D lerp_animator_3D;
+	lerp_animator_3D.SetValues(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetPosition(), m_CinematicObjectKeyFrames[m_CurrentKeyFrame+1]->GetPosition(),1.0f,Math::FUNC_CONSTANT);
+	Math::Vect3f pos;
+	lerp_animator_3D.Update(l_CurrentP, pos);
+
+	Math::CLerpAnimator1D lerp_animator_1D;
+	lerp_animator_1D.SetValues(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetYaw(), m_CinematicObjectKeyFrames[m_CurrentKeyFrame+1]->GetYaw(),1.0f,Math::FUNC_CONSTANT);
+	float yaw;
+	lerp_animator_1D.Update(l_CurrentP, yaw);
+
+	lerp_animator_1D.SetValues(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetPitch(), m_CinematicObjectKeyFrames[m_CurrentKeyFrame+1]->GetPitch(),1.0f,Math::FUNC_CONSTANT);
+	float pitch;
+	lerp_animator_1D.Update(l_CurrentP, pitch);
+
+	lerp_animator_1D.SetValues(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetRoll(), m_CinematicObjectKeyFrames[m_CurrentKeyFrame+1]->GetRoll(),1.0f,Math::FUNC_CONSTANT);
+	float roll;
+	lerp_animator_1D.Update(l_CurrentP, roll);
+	
+	lerp_animator_3D.SetValues(m_CinematicObjectKeyFrames[m_CurrentKeyFrame]->GetScale(), m_CinematicObjectKeyFrames[m_CurrentKeyFrame+1]->GetScale(),1.0f,Math::FUNC_CONSTANT);
+	Math::Vect3f scale;
+	lerp_animator_3D.Update(l_CurrentP, scale);
+
+	m_RenderableObject->SetPosition(pos);
+	m_RenderableObject->SetYaw(yaw);
+	m_RenderableObject->SetPitch(pitch);
+	m_RenderableObject->SetRoll(roll);
+	m_RenderableObject->SetScale(scale);
 }
 
 void CCinematicObject::OnRestartCycle()
