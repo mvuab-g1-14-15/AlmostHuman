d5a91d9acd64455b6c15d71a728174d984006f25
Jaume <jaumegali@gmail.com>
2015-02-09 21:09:42 +0100

    Parcial TriggerManager class

--- mvuab/Code/PhysX/Triggers/TriggerManager.cpp
+++ mvuab/Code/PhysX/Triggers/TriggerManager.cpp
@@ -1,1 +1,110 @@
+//----------------------------------------------------------------------------------
+// CTriggerManager class
+// Author: Xavier Macipe Arias
+// 
+// Description:
+// Clase Manager de Triggers de PhysX.
+//----------------------------------------------------------------------------------
+
 #include "TriggerManager.h"
+#include "XML\XMLTreeNode.h"
+#include "PhysicsManager.h"
+#include "Actor\PhysicActor.h"
+#include "Utils\PhysicUserData.h"
+#include "Logger\Logger.h"
+
+CTriggerManager::CTriggerManager(): 
+m_Value(16),
+m_UserData()
+{
+        m_UserData.clear();
+}
+                                
+CTriggerManager::~CTriggerManager()
+{
+        Clear();
+}
+
+bool CTriggerManager::LoadXML( const std::string &FileName )
+{
+        m_FileName = FileName;
+        CXMLTreeNode newFile;
+        
+        if (!newFile.LoadFile( FileName.c_str() ))
+        {
+				//->AddNewLog(ELL_ERROR, "TRIGERMANAGER::LOADXML: ERROR loading the file %s.\n", FileName.c_str());
+                return false;
+        }
+
+        CXMLTreeNode  m = newFile["triggers"];
+        if (m.Exists())
+        {
+                int count = m.GetNumChildren();
+                std::string sname;
+                for (int i = 0; i < count; ++i)
+                {
+                        sname= m(i).GetName();
+                        if (sname=="trigger")
+                        {
+                                // UserData
+                                std::string l_sTriggerName = m(i).GetPszProperty("name","");
+                                CPhysicUserData * l_pUserData = new CPhysicUserData(l_sTriggerName);
+                                l_pUserData->SetName(l_sTriggerName);
+                                l_pUserData->SetPaint(m(i).GetBoolProperty("paint",true));
+                                CColor col(m(i).GetVect4fProperty("color",Vect4f(0.0f,1.0f,0.0f,1.0f)));
+                                l_pUserData->SetColor(col);
+                                l_pUserData->SetGroup(ECG_TRIGGERS);
+                                m_UserData.push_back( l_pUserData );
+                                // Actor
+                                CPhysicActor *l_pActorTrigger = new CPhysicActor(l_pUserData);
+                                std::string l_sType = m(i).GetPszProperty("shape","");
+                                if (l_sType == "Box")
+                                {
+                                        Vect3f l_Pos = m(i).GetVect3fProperty("position",v3fZERO);
+                                        Vect3f l_Size = m(i).GetVect3fProperty("size",v3fZERO);
+                                        int l_Group = m(i).GetIntProperty("group",1);
+
+                                        l_pActorTrigger->CreateBoxTrigger(l_Pos,l_Size,l_Group);
+                                }
+                                else if (l_sType == "Sphere")
+                                        l_pActorTrigger->CreateSphereTrigger(m(i).GetVect3fProperty("position",v3fZERO),m(i).GetFloatProperty("radius",0.f),m(i).GetIntProperty("group",1));
+                                if (l_sType!="")
+                                {
+                                        int triggerGroup = m(i).GetIntProperty("group",1);
+                                        ECollisionGroup group = CORE->GetPhysicsManager()->GetCollisionGroup(triggerGroup);
+                                        l_pActorTrigger->SetTriggerGroup(group);
+                                        CORE->GetPhysicsManager()->AddPhysicActor(l_pActorTrigger);
+                                        //llevar esto a physicsManager
+                                        AddResource(l_sTriggerName, l_pActorTrigger);
+                                        // Trigger
+                                        sTrigger l_trigger;
+                                        l_trigger.enter = m(i).GetPszProperty("onEnter","",false);
+                                        l_trigger.exit = m(i).GetPszProperty("onExit","",false);
+                                        l_trigger.stay = m(i).GetPszProperty("onStay","",false);
+                                        m_LUAMap.insert( std::pair<std::string, sTrigger>(l_sTriggerName,l_trigger) );
+                                }
+                                else
+                                {
+                                        CHECKED_DELETE(l_pActorTrigger);
+                                }
+                        }
+                }
+        }
+        return true;
+}
+
+bool CTriggerManager::Reload()
+{
+        if(m_FileName!="")
+        {
+                Clear();
+                return LoadXML(m_FileName);
+        }
+        return false;
+}
+
+bool CTriggerManager::ReloadXML(const std::string &FileName)
+{
+        Clear();
+        return LoadXML(FileName);
+}
--- mvuab/Code/PhysX/Triggers/TriggerManager.h
+++ mvuab/Code/PhysX/Triggers/TriggerManager.h
@@ -1,9 +1,71 @@
-#ifndef _TRIGGER_MANAGER_H
-#define _TRIGGER_MANAGER_H
+//----------------------------------------------------------------------------------
+// CTriggerManager class
+// 
+// Description:
+// Clase Manager de Triggers de PhysX.
+//----------------------------------------------------------------------------------
 
-class CTriggerManager
+#pragma once
+
+#ifndef __TRIGGER_MANAGER_H__
+#define __TRIGGER_MANAGER_H__
+
+#include <string>
+#include <vector>
+#include <map>
+#include "Reports\PhysicTriggerReport.h"
+#include "Utils\TemplatedVectorMapManager.h"
+#include "Actor\PhysicActor.h"
+
+//---Forward Declarations---//
+class CPhysicUserData;
+class CXMLTreeNode;
+//--------------------------
+
+struct sTrigger
 {
-
+        std::string             enter;
+        std::string             exit;   
+        std::string             stay;   
 };
 
-#endif //_TRIGGER_MANAGER_H
+class CTriggerManager : public CTemplatedVectorMapManager<CPhysicActor>
+{
+public:
+        enum TTriggerType
+        {
+                ENTER=0,
+                LEAVE,
+                STAY
+        };
+
+private:
+        std::string                                     m_FileName;
+        std::vector     <CPhysicUserData *>             m_UserData;
+        std::map<std::string, sTrigger>					m_LUAMap;
+        
+        // Atributos para Luabind
+        int                                             m_Value;
+public:
+        CTriggerManager                                         ();
+        ~CTriggerManager                                        ();
+
+        bool                                                            LoadXML                                                 ( const std::string &FileName );
+        bool                                                            Reload                                                  ();
+        bool                                                            ReloadXML                                               ( const std::string &FileName );
+        std::vector<CPhysicActor*>                      GetTriggersVector                               ();
+        CPhysicActor*                                           GetTriggerByName                                ( std::string Name );
+        std::string                                                     GetLUAByName                                    ( std::string Name, u_int Type );
+        TTriggerType                                            GetTypeByName                                   ( std::string Name );
+        void                                                            Clear                                                   ();
+        void                                                            Release                                                 ();
+        void                                                            ReleaseTrigger                                  (const std::string &_trigger);
+        void                                                            AddTrigger                                              (CXMLTreeNode XMLData, ItemCore *Core, Vect3f Pos,sTrigger TriggerLua);
+        void                                                            AddTriggerWithoutXML                                            (std::string Name, ItemCore *Core, Vect3f Pos);
+
+        // Métodos para Luabind
+        int                                                             GetValue                                                () const;
+        void                                                            SetValue                                                ( const unsigned int& v ); 
+};
+
+#endif __TRIGGER_MANAGER_H__
