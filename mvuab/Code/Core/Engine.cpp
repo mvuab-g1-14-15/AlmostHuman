00e1f8807a4bf4af219b29d491e517b69a9aa480
Andrei Jianu <andreisystem@gmail.com>
2015-02-22 11:14:20 +0100

    Fix Release compiling and add the time spent on rendering in GPUStatics

--- mvuab/Code/Core/Engine.cpp
+++ mvuab/Code/Core/Engine.cpp
@@ -44,6 +44,7 @@ void CEngine::Render()
   m_pProcess->Render();
   m_pLogRender->Render();
   //pGraphicsManager->RenderCursor();
+
 #if _DEBUG
   pGraphicsManager->DisableZBuffering();
   pGraphicsManager->EnableAlphaBlend();
