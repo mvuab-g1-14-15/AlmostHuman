d25a9f6cd840cb0d1b5b16896f55682d7a4074f8
Andrei Jianu <andreisystem@gmail.com>
2015-02-27 01:29:40 +0100

    Using 2 textures for Gaussian commad

--- mvuab/Code/Graphics/SceneRenderComands/GaussianSceneRendererCommand.h
+++ mvuab/Code/Graphics/SceneRenderComands/GaussianSceneRendererCommand.h
@@ -11,12 +11,15 @@ class CGraphicsManager;
 class CGaussianSceneRendererCommand : public CStagedTexturedRendererCommand
 {
     private:
+        CTexture *m_pAuxTexture[2];
+
         std::string m_NameTechnique;
         unsigned int m_nIteration;
-        bool m_TextureAsFB;
         
         unsigned int m_Width;
         unsigned int m_Height;
+
+        
 
     public:
         CGaussianSceneRendererCommand( CXMLTreeNode& atts );
