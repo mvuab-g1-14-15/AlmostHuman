a9a1bc15e6dcb10d45f135ebe47d12d5217b23f8
Aljenci <alexjenci@gmail.com>
2015-03-06 17:45:29 +0100

    Initial execution of FSM

--- mvuab/bin/Videogame/Data/scripts/init/ai.lua
+++ mvuab/bin/Videogame/Data/scripts/init/ai.lua
@@ -1,13 +1,22 @@
 function set_initial_waypoint()
-	--TODO RAUL
-	--enemy = Enemy_Manager:GetActualEnemy()
-	--enemy:setTargetPosition(Vect3f(10,0,10))
+	core = Singleton_Core.get_singleton()
+	Enemy_Manager = core:GetEnemyManager()
+	enemy = Enemy_Manager:GetActualEnemy()
+	enemy:setTargetPosition(Vect3f(10,0,10))
 end
 
 function change_state()
-	--enemy = Enemy_Manager:GetActualEnemy()
-	--core = Singleton_Core.get_singleton()
-	--physicManager = core:GetPhysicManager()
-	--local character_controller_UserData = physicManager:GetUserData("CharacterController")
-	--local character_controller = character_controller_UserData:GetController()
+	core = Singleton_Core.get_singleton()
+	Enemy_Manager = core:GetEnemyManager()
+	enemy = Enemy_Manager:GetActualEnemy()
+	physicManager = core:GetPhysicsManager()
+	local character_controller_UserData = physicManager:GetUserData("CharacterController")
+	local character_controller = character_controller_UserData:GetController()
+	local character_pos = character_controller:GetPosition()
+	
+	local vector_dist = character_pos - enemy:GetPosition()
+	
+	if (vector_dist.x*vector_dist.x+vector_dist.y*vector_dist.y+vector_dist.z*vector_dist.z) < 10 then
+		enemy:setTargetPosition(character_pos)
+	end
 end
