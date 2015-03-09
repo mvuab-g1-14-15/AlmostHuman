2b6ed9ac8005b75db18d59e82c3b9959847f79d2
Aljenci <alexjenci@gmail.com>
2015-03-09 23:52:16 +0100

    First implementation of AStar

--- mvuab/Code/AI/Pathfinding/AStar.cpp
+++ mvuab/Code/AI/Pathfinding/AStar.cpp
@@ -48,30 +48,68 @@ CAStar::GetPath( Math::Vect3f init_pos, Math::Vect3f final_pos )
   std::vector<Math::Vect3f> path;
   path.push_back( init_pos );
 
-  Math::Vect3f first_point = m_Graph->GetNodeInfo( 0 );
-  Math::Vect3f last_point = m_Graph->GetNodeInfo( 0 );
+  unsigned int first_point = 0,
+               last_point = 0;
 
-  Math::Vect3f l_ActualNode;
   float l_ActualNodeDistance;
 
   for ( unsigned int i = 1; i < m_Graph->GetSize(); ++i )
   {
-    l_ActualNode = m_Graph->GetNodeInfo( i );
+    l_ActualNodeDistance = init_pos.Distance( m_Graph->GetNodeInfo( i ) );
 
-    l_ActualNodeDistance = init_pos.Distance( l_ActualNode );
+    if ( l_ActualNodeDistance < init_pos.Distance( m_Graph->GetNodeInfo( first_point ) ) )
+      first_point = i;
 
-    if ( l_ActualNodeDistance < init_pos.Distance( first_point ) )
-      first_point = l_ActualNode;
+    l_ActualNodeDistance = final_pos.Distance( m_Graph->GetNodeInfo( i ) );
 
-    l_ActualNodeDistance = final_pos.Distance( l_ActualNode );
-
-    if ( l_ActualNodeDistance < final_pos.Distance( last_point ) )
-      last_point = l_ActualNode;
+    if ( l_ActualNodeDistance < final_pos.Distance( m_Graph->GetNodeInfo( last_point ) ) )
+      last_point = i;
   }
 
-  path.push_back( first_point );
+  path.push_back( m_Graph->GetNodeInfo( first_point ) );
 
-  path.push_back( last_point );
+  std::vector<unsigned int> l_Visited;
+  unsigned int l_ActualNode = first_point;
+
+  bool l_PathFound = false;
+
+  float h = 0;
+  float g = 0;
+
+  while ( l_Visited.size() < m_Graph->GetSize() && !l_PathFound )
+  {
+    std::map<unsigned int, unsigned int> l_Neighbours;
+    l_Neighbours = m_Graph->GetArcs( l_ActualNode );
+
+    std::map<unsigned int, unsigned int>::iterator it = l_Neighbours.begin(),
+                                                   it_end = l_Neighbours.end();
+    float l_F = 9999999999;
+    unsigned int l_NextNode;
+
+    for ( ; it != it_end; ++it )
+    {
+      float l_H = h + m_Graph->GetNodeInfo( l_ActualNode ).Distance( m_Graph->GetNodeInfo( it->first ) );
+      float l_G = m_Graph->GetNodeInfo( l_ActualNode ).Distance( final_pos );
+
+      if ( l_H + l_G < l_F )
+      {
+        l_F = l_H + l_G;
+        l_NextNode = it->first;
+      }
+    }
+
+    l_Visited.push_back( l_ActualNode );
+    l_ActualNode = l_NextNode;
+
+    if ( l_ActualNode == last_point )
+      l_PathFound = true;
+  }
+
+  for ( unsigned int i = 1; i < l_Visited.size() - 1; ++i )
+    path.push_back( m_Graph->GetNodeInfo( i ) );
+
+  path.push_back( m_Graph->GetNodeInfo( last_point ) );
+
   path.push_back( final_pos );
   return path;
 }
