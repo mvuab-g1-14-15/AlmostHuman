69476e4ca06558f382a7b1e29848752512de5beb
Aljenci <alexjenci@gmail.com>
2015-03-10 00:07:13 +0100

    Fixed comment

--- mvuab/Code/AI/Pathfinding/AStar.cpp
+++ mvuab/Code/AI/Pathfinding/AStar.cpp
@@ -77,8 +77,8 @@ void CAStar::Render()
   function reconstruct_path(came_from,current)
     total_path := [current]
     while current in came_from:
-    current := came_from[current]
-    total_path.append(current)
+      current := came_from[current]
+      total_path.append(current)
     return total_path
 */
 
