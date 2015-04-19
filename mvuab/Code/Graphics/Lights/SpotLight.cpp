a815ed11a7b2cdfd2ebdbdcd2a517d5a9194cf88
Andrei Jianu <andreisystem@gmail.com>
2015-04-19 10:55:18 +0200

    Fix shadow map
    Culling frustum was not updated with the proper matrix

--- mvuab/Code/Graphics/Lights/SpotLight.cpp
+++ mvuab/Code/Graphics/Lights/SpotLight.cpp
@@ -4,6 +4,10 @@
 #include "EngineManagers.h"
 #include "Effects\EffectManager.h"
 
+#include "Cameras\Camera.h"
+#include "Cameras\CameraManager.h"
+
+
 #define D3DFVF_CUSTOMVERTEXLIGHT (D3DFVF_XYZ|D3DFVF_DIFFUSE)
 typedef struct CUSTOMVERTEXLIGHT
 {
@@ -63,10 +67,13 @@ void CSpotLight::SetShadowMap( CGraphicsManager* GM )
     D3DXMATRIX l_View;
     D3DXMATRIX l_Projection;
     D3DXVECTOR3 l_Eye( m_Position.x, m_Position.y, m_Position.z );
+
     m_Direction.Normalize();
     Vect3f l_LookAtV3f = m_Position + m_Direction;
+
     D3DXVECTOR3 l_LookAt( l_LookAtV3f.x, l_LookAtV3f.y, l_LookAtV3f.z );
     D3DXVECTOR3 l_VUP;
+
     float l_Value = Math::v3fY * l_LookAtV3f;
 
     if ( ( Math::v3fY * m_Direction ) > 0.995f || ( Math::v3fNEGY * m_Direction ) > 0.995f )
@@ -85,11 +92,16 @@ void CSpotLight::SetShadowMap( CGraphicsManager* GM )
   l_VUP.x = 0.0f;
   l_VUP.y = 1.0f;
   l_VUP.z = 0.0f;
+
   D3DXVec3Normalize( &l_VUP, &l_VUP );
   D3DXMatrixLookAtLH( &l_View, &l_Eye, &l_LookAt, &l_VUP );
   m_ViewShadowMap = Math::Mat44f( l_View );
+
   D3DXMatrixPerspectiveFovLH( &l_Projection, m_FallOff, 1.0f, 1.0f, m_EndRangeAttenuation );
   m_ProjectionShadowMap = Math::Mat44f( l_Projection );
+
   CEffectManager* l_EffectManager = EffectManagerInstance;
   l_EffectManager->ActivateCamera( m_ViewShadowMap, m_ProjectionShadowMap, m_Position );
+
+  CEngineManagers::GetSingletonPtr()->GetCameraManager()->GetCurrentCamera()->UpdateFrustum(l_View * l_Projection);
 }
