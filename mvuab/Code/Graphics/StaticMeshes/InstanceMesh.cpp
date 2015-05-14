7a5ac83cba277d3f7265f8e7b64dc726657b21df
iFuSiiOnzZ <andreisystem@gmail.com>
2015-05-14 16:46:10 +0200

    Creating physics from Vertex Buffer and Index Buffer
    En el debugger de PhysX actualmente no se puede ver nada, por lo tanto no puedo decir que funciona bien. La creación del actor se tiene que hacer a fuera para poder meterle un rigid body si queremos que ese objeto se pueda mover o no. Aun hace falta trabajo.

--- mvuab/Code/Graphics/StaticMeshes/InstanceMesh.cpp
+++ mvuab/Code/Graphics/StaticMeshes/InstanceMesh.cpp
@@ -9,23 +9,27 @@
 #include "Cameras/Frustum.h"
 #include "Cameras/CameraManager.h"
 
+#include "NxPhysicsSDK.h"
+#include "PhysicsManager.h"
 #include "EngineManagers.h"
 
-#include "Actor/PhysicActor.h"
+#include "NxPhysics.h"
+#include "NxCooking.h"
+
+#include "NxTriangleMesh.h"
+#include "NxTriangleMeshDesc.h"
+
+#include "Actor\PhysicActor.h"
+#include "Utils\PhysicStream.h"
+#include "Utils\PhysicUserData.h"
 
 
-CInstanceMesh::CInstanceMesh( const std::string& aName )
-    : CRenderableObject()
-    , mStaticMesh( 0 ) 
-    , mPhysicActor( 0 )
+CInstanceMesh::CInstanceMesh( const std::string& aName ) : CRenderableObject(), mStaticMesh( 0 ) 
 {
-    SetName( aName );
+    SetName(aName);
 }
 
-CInstanceMesh::CInstanceMesh( const std::string& Name, const std::string& CoreName )
-    : mStaticMesh( SMeshMInstance->GetResource(CoreName ) )
-    , CRenderableObject()
-    , mPhysicActor( 0 )
+CInstanceMesh::CInstanceMesh(const std::string& aName, const std::string& CoreName) : mStaticMesh(SMeshMInstance->GetResource(CoreName)) , CRenderableObject()
 {
     ASSERT( mStaticMesh, "Null static mesh" );
     
@@ -42,17 +46,61 @@ CInstanceMesh::CInstanceMesh( const std::string& Name, const std::string& CoreNa
         {
             m_VB[i] = l_Transform * m_VB[i];
         }
+
+        NxTriangleMeshDesc l_TriangleMeshDesc;
+
+        l_TriangleMeshDesc.numVertices      = ( NxU32 ) m_VB.size();
+        l_TriangleMeshDesc.numTriangles     = ( NxU32 ) m_IB.size() / 3;
+        
+        l_TriangleMeshDesc.pointStrideBytes     = sizeof( Math::Vect3f );
+        l_TriangleMeshDesc.triangleStrideBytes  = 3 * sizeof( uint32 );
+        
+        l_TriangleMeshDesc.points         = &m_VB[0].x;
+        l_TriangleMeshDesc.triangles      = &m_IB[0];
+        l_TriangleMeshDesc.flags          = 0;
+        
+        CPhysicMemoryWriteBuffer buf;
+        NxCookingInterface *l_pCooking = NxGetCookingLib( NX_PHYSICS_SDK_VERSION );
+        
+        if(l_pCooking->NxCookTriangleMesh(l_TriangleMeshDesc, buf))
+        {
+            NxTriangleMesh* l_TriangleMesh = PhysXMInstance->GetPhysicsSDK()->createTriangleMesh(CPhysicMemoryReadBuffer(buf.data));
+            
+            if(l_TriangleMesh != 0)
+            {
+                CPhysicUserData* l_pPhysicUserDataASEMesh = new CPhysicUserData(aName);
+                CPhysicActor* l_MeshActor = new CPhysicActor(l_pPhysicUserDataASEMesh);
+
+                l_MeshActor->AddMeshShape(l_TriangleMesh, Math::Vect3f());
+                bool oK = false;
+                
+                if(PhysXMInstance->CMapManager<CPhysicActor>::GetResource(aName) == 0)
+                {
+                    if(PhysXMInstance->AddPhysicActor(l_MeshActor))
+                    {
+                        PhysXMInstance->CMapManager<CPhysicActor>::AddResource(aName, l_MeshActor);
+                        oK = TRUE;
+                    }
+                }
+
+                if(!oK)
+                {
+                    CHECKED_DELETE(l_MeshActor);
+                    CHECKED_DELETE(l_pPhysicUserDataASEMesh);
+                }
+            }
+        }
     }
 
-    SetName( Name );
+    SetName( aName );
 }
 
 CInstanceMesh::CInstanceMesh( CXMLTreeNode& atts ) : CRenderableObject( atts ), mStaticMesh( SMeshMInstance->GetResource(atts.GetPszProperty( "core", "no_staticMesh" ) ) )
 {
     if(mStaticMesh != 0)
     {
-        m_VB = mStaticMesh->GetVB();
-        m_IB = mStaticMesh->GetIB();
+        std::vector<Math::Vect3f> m_VB = mStaticMesh->GetVB();
+        std::vector<uint32> m_IB = mStaticMesh->GetIB();
 
         Math::Mat44f l_Transform = GetTransform();
         int l_VecElements = m_VB.size();
@@ -61,6 +109,50 @@ CInstanceMesh::CInstanceMesh( CXMLTreeNode& atts ) : CRenderableObject( atts ), 
         for(int i = 0; i < l_VecElements; ++i)
         {
             m_VB[i] = l_Transform * m_VB[i];
+        }
+
+        NxTriangleMeshDesc l_TriangleMeshDesc;
+
+        l_TriangleMeshDesc.numVertices      = ( NxU32 ) m_VB.size();
+        l_TriangleMeshDesc.numTriangles     = ( NxU32 ) m_IB.size() / 3;
+        
+        l_TriangleMeshDesc.pointStrideBytes     = sizeof( Math::Vect3f );
+        l_TriangleMeshDesc.triangleStrideBytes  = 3 * sizeof( uint32 );
+        
+        l_TriangleMeshDesc.points         = &m_VB[0].x;
+        l_TriangleMeshDesc.triangles      = &m_IB[0];
+        l_TriangleMeshDesc.flags          = 0;
+        
+        CPhysicMemoryWriteBuffer buf;
+        NxCookingInterface *l_pCooking = NxGetCookingLib( NX_PHYSICS_SDK_VERSION );
+        
+        if(l_pCooking->NxCookTriangleMesh(l_TriangleMeshDesc, buf))
+        {
+            NxTriangleMesh* l_TriangleMesh = PhysXMInstance->GetPhysicsSDK()->createTriangleMesh(CPhysicMemoryReadBuffer(buf.data));
+            
+            if(l_TriangleMesh != 0)
+            {
+                CPhysicUserData* l_pPhysicUserDataASEMesh = new CPhysicUserData(GetName());
+                CPhysicActor* l_MeshActor = new CPhysicActor(l_pPhysicUserDataASEMesh);
+
+                l_MeshActor->AddMeshShape(l_TriangleMesh, Math::Vect3f());
+                bool oK = false;
+                
+                if(PhysXMInstance->CMapManager<CPhysicActor>::GetResource(GetName()) == 0)
+                {
+                    if(PhysXMInstance->AddPhysicActor(l_MeshActor))
+                    {
+                        PhysXMInstance->CMapManager<CPhysicActor>::AddResource(GetName(), l_MeshActor);
+                        oK = TRUE;
+                    }
+                }
+
+                if(!oK)
+                {
+                    CHECKED_DELETE(l_MeshActor);
+                    CHECKED_DELETE(l_pPhysicUserDataASEMesh);
+                }
+            }
         }
     }
 }
@@ -86,11 +178,4 @@ void CInstanceMesh::Render()
 
     Math::Mat44f t;
     GraphicsInstance->SetTransform( t );
-}
-
-bool CInstanceMesh::Init( CPhysicActor* aActor, CStaticMesh* aStaticMesh )
-{
-    mPhysicActor = aActor;
-    mStaticMesh = aStaticMesh;
-    return mStaticMesh != 0;
 }
--- mvuab/Code/Graphics/StaticMeshes/InstanceMesh.h
+++ mvuab/Code/Graphics/StaticMeshes/InstanceMesh.h
@@ -13,30 +13,21 @@ class CPhysicActor;
 class CInstanceMesh : public CRenderableObject
 {
     public:
-        CInstanceMesh(const std::string& Name, const std::string &CoreName);
-        CInstanceMesh( const std::string& aName );
+        CInstanceMesh(const std::string& aName, const std::string &CoreName);
+        CInstanceMesh(const std::string& aName);
+
         CInstanceMesh(CXMLTreeNode& atts);
         ~CInstanceMesh();
 
-        std::vector<Math::Vect3f> GetVertices() { return m_VB; }
-        std::vector<uint32> GetIndices() { return m_IB; }
-        
-        bool Init( CPhysicActor* aActor, CStaticMesh* aStaticMesh );
-
         void Render();
         inline CStaticMesh* GetStaticMesh();
+
     private:
         CStaticMesh*        mStaticMesh;
-        CPhysicActor*       mPhysicActor;
 
         std::vector<Math::Vect3f> m_VB;
         std::vector<uint32> m_IB;
 };
 
-//--------------------------------------------------------------------------------
-inline CStaticMesh* CInstanceMesh::GetStaticMesh()
-{
-    return mStaticMesh;
-}
 
 #endif //INC_INSTANCE_MESH_H_
