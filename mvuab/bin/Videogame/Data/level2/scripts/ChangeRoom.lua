7114b866fdc146eaa34b50bed098192ea0596afa
Raúl García Gaspar <raulgg1987@gmail.com>
2015-05-24 20:39:22 +0200

    Render DeveloperInfo & Weapon

--- mvuab/bin/Videogame/Data/level2/scripts/ChangeRoom.lua
+++ mvuab/bin/Videogame/Data/level2/scripts/ChangeRoom.lua
@@ -1,5 +1,5 @@
 g_Rooms = {Vect3f( -0.8, 0.55, 4.87 ), Vect3f( 8, -12.5, 18 ), Vect3f( 40, -18.5, -8), Vect3f( 141, 31.5, -17 ), Vect3f( 104, 20.5, 198 )}
-g_ActualRoom = 4
+g_ActualRoom = 1
 
 function ChangeRoom()
     g_ActualRoom = g_ActualRoom + 1
