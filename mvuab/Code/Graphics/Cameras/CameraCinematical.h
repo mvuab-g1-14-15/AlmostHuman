ec42bddc8655513143fa6462cebff105ed04feef
Vazquinhos <vazquinhos@gmail.com>
2015-07-23 08:08:01 +0200

    Correct all the unknown textures

--- mvuab/Code/Graphics/Cameras/CameraCinematical.h
+++ mvuab/Code/Graphics/Cameras/CameraCinematical.h
@@ -2,29 +2,38 @@
 #define INC_CAMERACINEMATICAL_H_
 
 #include "Camera.h"
-#include <d3dx9math.h>
+
 
 class CCameraKeyController;
 
-class CCameraCinematical : public CCamera 
+class CCameraCinematical : public CCamera
 {
-public:
-    CCameraCinematical(CCameraKeyController *KeyController);
-    ~CCameraCinematical();
-    void    Update( );
-        
-    Math::Vect3f GetDirection( void ) const { return GetLookAt() - m_Position;}
+    public:
+        CCameraCinematical(CCameraKeyController *KeyController);
+        ~CCameraCinematical();
+        void    Update( );
 
-    void SetLookAt( const Math::Vect3f & LookAt  ){m_LookAt = LookAt;}
-    Math::Vect3f GetLookAt( void ) const;
+        Math::Vect3f GetDirection( void ) const
+        {
+            return GetLookAt() - m_Position;
+        }
 
-    void SetVecUp( const Math::Vect3f & Up){m_Up = Up;}
-    Math::Vect3f GetVecUp( void ) const;
-    
-private: // Members
-    CCameraKeyController *m_pKeyController;
-    Math::Vect3f                    m_LookAt;
-    Math::Vect3f                    m_Up;
+        void SetLookAt( const Math::Vect3f & LookAt  )
+        {
+            m_LookAt = LookAt;
+        }
+        Math::Vect3f GetLookAt( void ) const;
+
+        void SetVecUp( const Math::Vect3f & Up)
+        {
+            m_Up = Up;
+        }
+        Math::Vect3f GetVecUp( void ) const;
+
+    private: // Members
+        CCameraKeyController *m_pKeyController;
+        Math::Vect3f                    m_LookAt;
+        Math::Vect3f                    m_Up;
 };
 
 #endif //INC_CAMERAFPSHOOTER_H_
