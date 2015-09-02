c362e9ae5965d71ca33cba08571bebc6a7adf473
galix <jaumegali@gmail.com>
2015-09-02 19:21:00 +0200

    New Sala3 Imported

--- mvuab/bin/Videogame/Data/level2/scripts/Init.lua
+++ mvuab/bin/Videogame/Data/level2/scripts/Init.lua
@@ -14,8 +14,8 @@ initialized1 = false
 function load_basics()
 	-- basic loads
 	--scene:ActivateRoom("room1")
-	scene:ActivateRoom("room2")
-	--scene:ActivateRoom("room3")
+	--scene:ActivateRoom("room2")
+	scene:ActivateRoom("room3")
 end
 
 function load_gameplay()
