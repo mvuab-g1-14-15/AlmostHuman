43bb112ee2c0ed5f648e1b43bcab024a3097aa72
Andrei Jianu <andreisystem@gmail.com>
2015-09-15 18:13:22 +0200

    Change room1 to sala1 en Player.lua

--- mvuab/bin/Videogame/Data/level2/scripts/Player.lua
+++ mvuab/bin/Videogame/Data/level2/scripts/Player.lua
@@ -22,7 +22,7 @@ function CPlayer:__init()
 	self.RenderableObject = renderable_objects_manager_characters:GetResource("Player")
 	if self.RenderableObject == nil then
 		self.RenderableObject = CreateAnimatedInstanceModel("Player", "Player")
-		self.RenderableObject:SetRoomName( "room2" )
+		self.RenderableObject:SetRoomName( "sala1" )
 		renderable_objects_manager_characters:AddResource("Player", self.RenderableObject)
 	end
 
