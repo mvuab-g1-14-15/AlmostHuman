9c05f3fa69932eb25a8c5a573bd1a2360f089504
Alejandro Jiménez Encinas <alexjenci@gmail.com>
2015-10-04 12:45:05 +0200

    Pasillo casca geometria, no esta reexportado

--- mvuab/bin/Videogame/Data/level2/scripts/Init.lua
+++ mvuab/bin/Videogame/Data/level2/scripts/Init.lua
@@ -15,7 +15,7 @@ function load_basics()
 	engine:Trace("Init the load_basics()")
 	-- basic loads
 	scene:ActivateRoom("sala1")
-	scene:ActivateRoom("pasillo")
+	--scene:ActivateRoom("pasillo")
 	--scene:ActivateRoom("sala4")
 	--scene:ActivateRoom("sala2")
 	--scene:ActivateRoom("sala3")
