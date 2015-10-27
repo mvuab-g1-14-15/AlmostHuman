142ffd52f7a3b0a0ed7b8e71d3b749a746e2bb22
galix <jaumegali@gmail.com>
2015-10-27 19:13:20 +0100

    Added space room

--- mvuab/bin/Videogame/Data/scripts/Init.lua
+++ mvuab/bin/Videogame/Data/scripts/Init.lua
@@ -28,7 +28,7 @@ function load_basics()
 	scene:ActivateRoom("sala3")	
 	scene:ActivateRoom("elevator")	
 	scene:ActivateRoom("sala4")	
-	
+	light_manager:SetAmbientLight( Vect3f(0.5, 0.5,0.5))
 	engine:Trace("Finish the load_basics()")
 end
 
