ea65d7efbaac9eda8e513110d1027a55063f6637
Vazquinhos <vazquinhos@gmail.com>
2015-10-30 16:44:49 +0100

    Lua particle corrected

--- mvuab/bin/Videogame/Data/scripts/Particle.lua
+++ mvuab/bin/Videogame/Data/scripts/Particle.lua
@@ -1,26 +1,26 @@
 class 'CParticle'
 
-function CParticle:__init( aCoreName )
-	--self.Instance = CParticleInstance( "", aCoreName, "core" );
-	--self.Instance:ChangeVisibility( false );
+function CParticle:__init( aName, aCoreName, aRoomName )
+	self.Instance = particle_system_manager:GetInstance( aName, aCoreName, aRoomName );
+	self.Instance:ChangeVisibility( false );
 end
 
 function CParticle:Init( aPosition )
-	--self:ChangePosition( aPosition )
-	--self.Core:ResetEmitters();
-	--self.Instance:ChangeVisibility( true )
+	self.Instance:ChangePosition( aPosition );
+	self.Instance:Reset();
+	self.Instance:ChangeVisibility( true )
 end
 
 function CParticle:Begin( aPosition )
-	--self.Instance:ChangeVisibility( true )
-	--self:ChangePosition( aPosition )
+	self.Instance:ChangeVisibility( true )
+	self:ChangePosition( aPosition )
 end
 
 function CParticle:ChangePosition( aPosition )
-	--self.Instance:ChangePosition( aPosition )
+	self.Instance:ChangePosition( aPosition )
 end
 
 function CParticle:SetDirection( aDir )
-	--self.Core:SetFixedDirection( aDir )
+	self.Instance:SetDirection( aDir )
 end
 
